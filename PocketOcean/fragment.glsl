2e17f3edae472cb83e63c0ee27d3b2e7735cb52f
Davis Robertson <davis@daviskr.com>
2018-03-01 04:28:24 -0600

    Fix specular calculation in shaders

--- PocketOcean/fragment.glsl
+++ PocketOcean/fragment.glsl
@@ -1,26 +1,25 @@
 precision mediump float;
 
 varying vec3 normalInterp;
-varying vec3 vertPos;
+varying vec4 worldPos;
 
 const vec3 lightPos 	= vec3(200,60,100);
 const vec3 ambientColor = vec3(0.0, 0, 0.1);
 const vec3 diffuseColor = vec3(0, .4666 , .745);
 const vec3 specColor 	= vec3(1.0, 1.0, 1.0);
 
+
 void main() {
+
 	vec3 normal = normalize(normalInterp);
-	vec3 lightDir = normalize(lightPos - vertPos);
+	vec3 lightDir = normalize(lightPos - worldPos.xyz);
 
 	float lambertian = max(dot(lightDir,normal), 0.0);
 	float specular = 0.0;
 
-    if(lambertian > 0.0){
-        vec4 r = vec4(normalize(reflect(lightDir, normal)), 0.0);
-        vec4 v = normalize(viewMatrix * vec4(cameraPosition - vertPos, 0.0));
-        specular = pow(clamp(max(dot(v, r), 0.0), 0.0, 1.0), 20.0);
-    }
-
+	vec3 r = normalize(reflect(-lightDir, normal));
+    vec3 v = normalize(cameraPosition - worldPos.xyz);
+    specular = pow(clamp(max(dot(v, r), 0.0), 0.0, 1.0), 12.0);
 
 	gl_FragColor = vec4(ambientColor + lambertian * diffuseColor + specular * specColor, 1.0);
 }
--- PocketOcean/vertex.glsl
+++ PocketOcean/vertex.glsl
@@ -1,6 +1,6 @@
 uniform float time;
 varying vec3 normalInterp;
-varying vec3 vertPos;
+varying vec4 worldPos;
 
 float vertexHeight(float x, float z, vec2 direction, float freq, float t, float speed, float amplitude) {
 	return amplitude * sin(dot(direction, vec2(x,z)) * freq + t * speed * freq);
@@ -10,19 +10,18 @@ vec4 vertexNormal(float x, float z, vec2 direction, float freq, float t, float s
 	float d = freq * amplitude * cos(dot(direction, vec2(x,z)) * freq + time * speed * freq);
 	float dx = direction.x * d;
 	float dz = direction.y * d;
-	return vec4(-dx, 1.0, -dz, 0.0);
+	return vec4(-dx, 0.0, -dz, 0.0);
 }
 
 void main(){
     vec4 wpos = vec4(position, 1.0);
     wpos.z += vertexHeight(position.x,  position.y, vec2(1.0, 0.0), 0.5, time, 2.5, 0.5);
     wpos.z += vertexHeight(position.x,  position.y, normalize(vec2(1.0, 1.0)), 1.0, time, 1.0, 0.65);
-    vec4 wnorm = vec4(0,0,0,0);
+    vec4 wnorm = vec4(0,1,0,0);
     wnorm += vertexNormal(position.x,  position.y, vec2(1.0, 0.0), 0.5, time, 2.0, 0.5);
     wnorm += vertexNormal(position.x,  position.y, normalize(vec2(1.0, 1.0)), 1.0, time, 1.0, 0.65);
-    gl_Position = projectionMatrix * modelViewMatrix * wpos;
-    vec4 vertPos4 = modelViewMatrix * wpos;
 
-    vertPos = vec3(vertPos4) / vertPos4.w;
+    gl_Position = projectionMatrix * modelViewMatrix * wpos;
+    worldPos = wpos;
     normalInterp =  normalize(wnorm.xyz);
 }
