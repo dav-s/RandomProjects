fb809664d265ac996b5ca183fd51d18f3238e9a7
Davis Robertson <davis@daviskr.com>
2018-03-04 02:12:07 -0600

    Factor out pocket.gl params into params.js

---
+++ PocketOcean/params.js
@@ -0,0 +1,17 @@
+{
+    vertexShaderFile:"vertex.glsl",
+    fragmentShaderFile:"fragment.glsl",
+    meshes:[
+        {
+            type:"plane",
+            name:"Ocean",
+            rx:-90,
+            subdivision:128,
+            scale:1.5
+        }
+    ],
+    animated:true,
+    autoOrbit:true,
+    zoom:true,
+    cameraDistance:75
+}
